d64825d98fd485d67e9a64e89c91576885122507
Zeynep Bağcı <zeynepbag99@gmail.com>
2023-03-23 23:53:10 +0300

    ilk react proje kodları

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './style.css'
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h3>{process.env.NODE_ENVncl}</h3>
+      test
+      </div>
+    </div>
+  );
+}
+
+export default App;
